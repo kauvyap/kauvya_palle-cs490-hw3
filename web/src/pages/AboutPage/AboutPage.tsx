a8782a9d688aff9dac8ab8f057adcae4e6f9d7e3
Kauvya Palle <kpp82@njit.edu>
2023-09-16 17:10:43 -0400

    First commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,20 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <h1>AboutPage</h1>
+
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of redwood: Look on my
+          works, ye mighty, and despair~!
+        </p>
+      </main>
+    </>
+  )
+}
+
+export default AboutPage
